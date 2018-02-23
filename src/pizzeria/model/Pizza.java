c52ec1fbadcea0ceceac925b5990420cddb1c762
ETY0005 <ETY0005@DESKTOP-VS7UP0S>
2018-02-23 12:03:07 +0100

    add git ignore add class Pizerria

---
+++ src/pizzeria/model/Pizza.java
@@ -0,0 +1,32 @@
+package pizzeria.model;
+
+public class Pizza {
+	int id;
+	String code;
+	String libelle;
+	double prix;
+	
+	static int compteur = 0;
+	
+	public Pizza(String code, String libelle, double prix  ){
+		
+		compteur ++;
+		id = compteur;
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+		
+	}
+	
+	public Pizza(int id, String code, String libelle, double prix  ){
+		
+		
+		this.id = id;
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+		
+	}
+	
+
+}
