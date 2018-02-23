dd52be0876e480850e2295e84c9a0d866ff7213b
ETY0005 <ETY0005@DESKTOP-VS7UP0S>
2018-02-23 17:32:52 +0100

    Suite TP3 (finir suppression)

--- src/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,40 +1,129 @@
 package pizzeria.console;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
+import pizzeria.model.Pizza;
 
 public class PizzeriaAdminConsoleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stu
+
+		Pizza[] pizzas = new Pizza[8];
+
+		pizzas[0] = new Pizza(0, "PEP", "Pépéroni", 12.50);
+		pizzas[1] = new Pizza(1, "MAR", "Margherita", 14.00);
+		pizzas[2] = new Pizza(2, "REIN", "La Reine", 11.50);
+		pizzas[3] = new Pizza(3, "FRO", "La 4 fromages", 12.00);
+		pizzas[4] = new Pizza(4, "CAN", "La Cannibale", 12.50);
+		pizzas[5] = new Pizza(5, "SAV", "La savoyarde", 13.00);
+		pizzas[6] = new Pizza(6, "ORI", "L'orientale", 13.50);
+		pizzas[7] = new Pizza(7, "IND", "L'indienne", 14.00);
+
 		Scanner questionUser = new Scanner(System.in);
-		int sortie = 1;
-		while(sortie == 1){
-		System.out.println("1. Lister les pizzas");
-		System.out.println("2. Ajouter une nouvelle pizza");
-		System.out.println("3. Mettre à jour une pizza");
-		System.out.println("4. Supprimer une pizza");
-		System.out.println("99. Sortir");
-		
-		System.out.println("Choisissez une option dans le menu ci dessus");
-		
-		int choix = questionUser.nextInt() ;
-		
-		switch (choix) {
-        case 1:  System.out.println("Liste des pizzas");
-                 break;
-        case 2:  System.out.println("Ajout d'une nouvelel pizza");
-                 break;
-        case 3:  System.out.println("Suppression d'une pizza");
-                 break;
-        case 99:  System.out.println("Aurevoir");
-                  sortie = 2;
-                 break;
-		
+		int choix = 0;
+
+		do {
+			System.out.println("1. Lister les pizzas");
+			System.out.println("2. Ajouter une nouvelle pizza");
+			System.out.println("3. Mettre à jour une pizza");
+			System.out.println("4. Supprimer une pizza");
+			System.out.println("99. Sortir");
+
+			System.out.println("Choisissez une option dans le menu ci dessus");
+
+			choix = questionUser.nextInt();
+
+			switch (choix) {
+			case 1:
+				for (int i = 0; i < pizzas.length; i++) {
+					System.out.println(pizzas[i].getCode() + "->" + pizzas[i].getLibelle() + "(" + pizzas[i].getPrix()
+							+ "\u20ac" + ")");
+
+				}
+
+				break;
+			case 2:
+				System.out.println("Veuillez saisir le code:");
+				String code = questionUser.next();
+				System.out.println("Veuillez saisir le nom (sans espace):");
+				String lib = questionUser.next();
+				System.out.println("Veuillez saisir le prix:");
+				double prix = questionUser.nextDouble();
+
+				pizzas = Arrays.copyOf(pizzas, pizzas.length + 1);
+				pizzas[pizzas.length - 1] = new Pizza(code, lib, prix);
+
+				break;
+			case 3:
+				System.out.println("Modif d'une pizza");
+				for (int i = 0; i < pizzas.length; i++) {
+					System.out.println(pizzas[i].getCode() + "->" + pizzas[i].getLibelle() + "(" + pizzas[i].getPrix()
+							+ "\u20ac" + ")");
+
+				}
+				System.out.println("Veuillez saisir le code de la pizza à modifier:");
+				String codeModif = questionUser.next();
+				for (int i = 0; i < pizzas.length; i++) {
+					if (pizzas[i].getCode().equals(codeModif)) {
+
+						System.out.println("Veuillez saisir le nouveau code:");
+						String codeM = questionUser.next();
+						pizzas[i].setCode(codeM);
+
+						System.out.println("Veuillez saisir le nouveau nom (sans espace):");
+						String libM = questionUser.next();
+						pizzas[i].setLibelle(libM);
+
+						System.out.println("Veuillez saisir le prix:");
+						double prixM = questionUser.nextDouble();
+						pizzas[i].setPrix(prixM);
+
+					}
+				}
+
+				break;
+
+			case 4:
+				for (int i = 0; i < pizzas.length; i++) {
+					System.out.println(pizzas[i].getCode() + "->" + pizzas[i].getLibelle() + "(" + pizzas[i].getPrix()
+							+ "\u20ac" + ")");
+
+				}
+				System.out.println("Veuillez saisir le code de la pizza à supprimer:");
+				String codeSupr = questionUser.next();
+				int taillePizzas = pizzas.length;
+				Pizza [] pizzasCopy = new Pizza [pizzas.length - 1];
+				
+				for (int i = 0; i < pizzas.length; i++) {
+					if (pizzas[i].getCode().equals(codeSupr)) {
+						
+						for(int j=i+1; j<pizzas.length; j++)
+						{ 
+						pizzasCopy[j]=pizzas[j+1];
+							
+					        }
+						taillePizzas=taillePizzas-1;	
+						for(int k=0; k<taillePizzas; k++)
+						{System.out.println(pizzas[k].getCode() + "->" + pizzas[k].getLibelle() + "(" + pizzas[k].getPrix()
+								+ "\u20ac" + ")");
 
 
+					}
+				}
+				}
+					break;
+				
+				
 
-	}
-		}
+			case 99:
+				System.out.println("Aurevoir");
+
+				break;
+
+			}
+		} while (choix != 99);
 	}
 
 }
--- src/pizzeria/model/Pizza.java
+++ src/pizzeria/model/Pizza.java
@@ -27,6 +27,38 @@ public class Pizza {
 		this.prix = prix;
 		
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
 	
 
 }
